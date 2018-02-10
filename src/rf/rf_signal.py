0212b7ca587c98f7f340e561237b75f7b734236f
Fabio Hellmann <fhellmann93@googlemail.com>
2018-02-10 13:31:09 +0100

    Add __str__ for debug

--- src/rf/rf_signal.py
+++ src/rf/rf_signal.py
@@ -22,3 +22,7 @@ class RfSignal:
 
     def get_protocol(self):
         return self.rx_protocol
+
+    def __str__(self):
+        return "RF-Signal(time=" + str(self.get_time()) + ", code=" + str(self.get_code()) + \
+               ", pulselength=" + str(self.get_pulselength()) + ", protocol=" + str(self.get_protocol()) + ")"
