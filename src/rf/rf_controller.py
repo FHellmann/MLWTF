a784b183b90860fc59b7fa5c5356198c966283fc
Fabio Hellmann <fhellmann93@googlemail.com>
2018-02-10 13:02:29 +0100

    Fix subscriber pattern

--- src/rf/rf_controller.py
+++ src/rf/rf_controller.py
@@ -15,6 +15,7 @@ class RfController:
     def __init__(self):
         self.rfdevice = RFDevice(17)
         self.rfdevice.enable_tx()
+        self.subscribers = []
         threading.Thread(target=self.listening).start()
 
     def __del__(self):
@@ -23,7 +24,10 @@ class RfController:
     def send(self, rf_signal):
         self.rfdevice.send(rf_signal.get_code(), rf_signal.get_protocol(), rf_signal.get_pulselength())
 
-    def listening(self, callback):
+    def subscribe(self, subscriber):
+        self.subscribers.append(subscriber)
+
+    def listening(self):
         timestamp = None
         while True:
             if self.rfdevice.rx_code_timestamp != timestamp:
@@ -33,6 +37,7 @@ class RfController:
                 rx_proto = self.rfdevice.rx_proto
 
                 logging.info("Signal detected: " + str(rx_code) + " [pulselength " + str(rx_pulselength) + ", protocol " + str(rx_proto) + "]")
-                callback(RfSignal(timestamp, rx_code, rx_pulselength, rx_proto))
+                for subscriber in self.subscribers:
+                    subscriber(RfSignal(timestamp, rx_code, rx_pulselength, rx_proto))
 
             time.sleep(0.01)
