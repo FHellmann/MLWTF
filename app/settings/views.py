1db369c0e74aee130e9efdc0e472bd35855de1a0
Fabio Hellmann <fhellmann93@googlemail.com>
2018-02-10 23:23:28 +0100

    Add option to send test signal

--- app/settings/views.py
+++ app/settings/views.py
@@ -19,5 +19,22 @@ def rf_devices():
     """
     Render the rf-device template on the /rfdevices route
     """
-    print(rf_controller.get_signals())
     return render_template('settings/rfdevices.html', title="Settings", devices=rf_controller.get_signals())
+
+
+@settings.route('/rfdevices/add-signal', methods=['POST'])
+def add_signal(rf_signal_key):
+    """
+
+    """
+
+    return rf_devices()
+
+
+@settings.route('/rfdevices/send-signal', methods=['POST'])
+def send_signal(rf_signal_key):
+    """
+    Send the specific signal and return same site
+    """
+    rf_controller.send(rf_signal_key)
+    return rf_devices()
