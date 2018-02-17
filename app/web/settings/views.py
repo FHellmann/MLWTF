028c61d5b3e3b07a07b8ce24c136fee401d7cb6c
Fabio Hellmann <fhellmann93@googlemail.com>
2018-02-17 02:45:25 +0100

    Update services

--- app/web/settings/views.py
+++ app/web/settings/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for
 
 from . import settings
-from ...sensors.rx_service import RfController
+from ...sensors_service.rx_service import RfController
 
 rf_controller = RfController()
 
