90922e73b88b4231b73a4372ee2df8d6f01d61f8
Fabio Hellmann <fhellmann93@googlemail.com>
2018-02-17 02:42:52 +0100

    Refactor rf rpi

--- app/hardware/rf_rpi.py
+++ app/hardware/rf_rpi.py
@@ -5,32 +5,60 @@ Sending and receiving 433/315Mhz signals with low-cost GPIO RF Modules on a Rasp
 import logging
 import time
 from collections import namedtuple
-
+from enum import Enum
 from RPi import GPIO
 
 MAX_CHANGES = 67
 
 _LOGGER = logging.getLogger(__name__)
 
-Protocol = namedtuple('Protocol',
-                      ['pulselength',
-                       'sync_high', 'sync_low',
-                       'zero_high', 'zero_low',
-                       'one_high', 'one_low'])
-PROTOCOLS = (None,
-             Protocol(350, 1, 31, 1, 3, 3, 1),
-             Protocol(650, 1, 10, 1, 2, 2, 1),
-             Protocol(100, 30, 71, 4, 11, 9, 6),
-             Protocol(380, 1, 6, 1, 3, 3, 1),
-             Protocol(500, 6, 14, 1, 2, 2, 1))
+
+class Protocol:
+    def __init__(self, pulselength, sync_high, sync_low, zero_high, zero_low, one_high, one_low):
+        self.pulselength = pulselength
+        self.sync_high = sync_high
+        self.sync_low = sync_low
+        self.zero_high = zero_high
+        self.zero_low = zero_low
+        self.one_high = one_high
+        self.one_low = one_low
+
+    def __str__(self):
+        return __name__ + "(pulselength=" + str(self.pulselength) + ", sync_high=" + str(self.sync_high) + \
+               ", sync_low=" + str(self.sync_low) + ", zero_high=" + str(self.zero_high) + \
+               ", zero_low=" + str(self.zero_low) + ", one_high=" + str(self.one_high) + \
+               ", one_low=" + str(self.one_low) + ")"
 
 
-class RFDevice:
+class ProtocolType(Enum):
+    NONE = None
+    PL_350 = Protocol(350, 1, 31, 1, 3, 3, 1)
+    PL_650 = Protocol(650, 1, 10, 1, 2, 2, 1)
+    PL_100 = Protocol(100, 30, 71, 4, 11, 9, 6)
+    PL_380 = Protocol(380, 1, 6, 1, 3, 3, 1)
+    PL_500 = Protocol(500, 6, 14, 1, 2, 2, 1)
+
+
+class Signal:
+    def __init__(self, time, code, pulselength, bit_length, protocol):
+        self.time = time
+        self.code = code
+        self.pulselength = pulselength
+        self.bit_length = bit_length
+        self.protocol = protocol
+
+    def __str__(self):
+        return __name__ + "(time=" + str(self.time) + ", code=" + str(self.code) + \
+               ", pulselength=" + str(self.pulselength) + ", bitlength=" + str(self.bit_length) +\
+               ", protocol=" + str(self.protocol) + ")"
+
+
+class Device:
     """Representation of a GPIO RF device."""
 
     # pylint: disable=too-many-instance-attributes,too-many-arguments
     def __init__(self, gpio,
-                 tx_proto=1, tx_pulselength=None, tx_repeat=10, tx_length=24, rx_tolerance=80):
+                 tx_proto=ProtocolType.PL_350, tx_pulselength=None, tx_repeat=10, tx_length=24, rx_tolerance=80):
         """Initialize the RF device."""
         self.gpio = gpio
         self.tx_enabled = False
@@ -38,7 +66,7 @@ class RFDevice:
         if tx_pulselength:
             self.tx_pulselength = tx_pulselength
         else:
-            self.tx_pulselength = PROTOCOLS[tx_proto].pulselength
+            self.tx_pulselength = tx_proto.px_pulselength
         self.tx_repeat = tx_repeat
         self.tx_length = tx_length
         self.rx_enabled = False
@@ -49,11 +77,7 @@ class RFDevice:
         self._rx_change_count = 0
         self._rx_repeat_count = 0
         # successful RX values
-        self.rx_code = None
-        self.rx_code_timestamp = None
-        self.rx_proto = None
-        self.rx_bitlength = None
-        self.rx_pulselength = None
+        self.rx_signal = None
 
         GPIO.setmode(GPIO.BCM)
         _LOGGER.debug("Using GPIO " + str(gpio))
@@ -87,22 +111,22 @@ class RFDevice:
             _LOGGER.debug("TX disabled")
         return True
 
-    def tx_code(self, code, tx_proto=None, tx_pulselength=None):
+    def tx_code(self, signal):
         """
         Send a decimal code.
         Optionally set protocol and pulselength.
         When none given reset to default protocol and pulselength.
         """
-        if tx_proto:
-            self.tx_proto = tx_proto
+        if signal.protocol:
+            self.tx_proto = signal.protocol
         else:
-            self.tx_proto = 1
-        if tx_pulselength:
-            self.tx_pulselength = tx_pulselength
+            self.tx_proto = ProtocolType.PL_350
+        if signal.pulselength:
+            self.tx_pulselength = signal.pulselength
         else:
-            self.tx_pulselength = PROTOCOLS[self.tx_proto].pulselength
-        rawcode = format(code, '#0{}b'.format(self.tx_length + 2))[2:]
-        _LOGGER.debug("TX code: " + str(code))
+            self.tx_pulselength = self.tx_proto.pulselength
+        rawcode = format(signal.code, '#0{}b'.format(self.tx_length + 2))[2:]
+        _LOGGER.debug("TX code: " + str(signal.code))
         return self.tx_bin(rawcode)
 
     def tx_bin(self, rawcode):
@@ -123,27 +147,18 @@ class RFDevice:
 
     def tx_l0(self):
         """Send a '0' bit."""
-        if not 0 < self.tx_proto < len(PROTOCOLS):
-            _LOGGER.error("Unknown TX protocol")
-            return False
-        return self.tx_waveform(PROTOCOLS[self.tx_proto].zero_high,
-                                PROTOCOLS[self.tx_proto].zero_low)
+        return self.tx_waveform(self.tx_proto.zero_high,
+                                self.tx_proto.zero_low)
 
     def tx_l1(self):
         """Send a '1' bit."""
-        if not 0 < self.tx_proto < len(PROTOCOLS):
-            _LOGGER.error("Unknown TX protocol")
-            return False
-        return self.tx_waveform(PROTOCOLS[self.tx_proto].one_high,
-                                PROTOCOLS[self.tx_proto].one_low)
+        return self.tx_waveform(self.tx_proto.one_high,
+                                self.tx_proto.one_low)
 
     def tx_sync(self):
         """Send a sync."""
-        if not 0 < self.tx_proto < len(PROTOCOLS):
-            _LOGGER.error("Unknown TX protocol")
-            return False
-        return self.tx_waveform(PROTOCOLS[self.tx_proto].sync_high,
-                                PROTOCOLS[self.tx_proto].sync_low)
+        return self.tx_waveform(self.tx_proto.sync_high,
+                                self.tx_proto.sync_low)
 
     def tx_waveform(self, highpulses, lowpulses):
         """Send basic waveform."""
@@ -188,9 +203,9 @@ class RFDevice:
                 self._rx_repeat_count += 1
                 self._rx_change_count -= 1
                 if self._rx_repeat_count == 2:
-                    for pnum in range(1, len(PROTOCOLS)):
-                        if self._rx_waveform(pnum, self._rx_change_count, timestamp):
-                            _LOGGER.debug("RX code " + str(self.rx_code))
+                    for name, protocol in ProtocolType.__members__.items():
+                        if self._rx_waveform(protocol, self._rx_change_count, timestamp):
+                            _LOGGER.debug("RX code " + str(self.rx_signal.code))
                             break
                     self._rx_repeat_count = 0
             self._rx_change_count = 0
@@ -202,29 +217,25 @@ class RFDevice:
         self._rx_change_count += 1
         self._rx_last_timestamp = timestamp
 
-    def _rx_waveform(self, pnum, change_count, timestamp):
+    def _rx_waveform(self, protocol, change_count, timestamp):
         """Detect waveform and format code."""
         code = 0
-        delay = int(self._rx_timings[0] / PROTOCOLS[pnum].sync_low)
+        delay = int(self._rx_timings[0] / protocol.sync_low)
         delay_tolerance = delay * self.rx_tolerance / 100
 
         for i in range(1, change_count, 2):
-            if (self._rx_timings[i] - delay * PROTOCOLS[pnum].zero_high < delay_tolerance and
-                    self._rx_timings[i+1] - delay * PROTOCOLS[pnum].zero_low < delay_tolerance):
+            if (self._rx_timings[i] - delay * protocol.zero_high < delay_tolerance and
+                    self._rx_timings[i+1] - delay * protocol.zero_low < delay_tolerance):
                 code <<= 1
-            elif (self._rx_timings[i] - delay * PROTOCOLS[pnum].one_high < delay_tolerance and
-                  self._rx_timings[i+1] - delay * PROTOCOLS[pnum].one_low < delay_tolerance):
+            elif (self._rx_timings[i] - delay * protocol.one_high < delay_tolerance and
+                  self._rx_timings[i+1] - delay * protocol.one_low < delay_tolerance):
                 code <<= 1
                 code |= 1
             else:
                 return False
 
         if self._rx_change_count > 6 and code != 0:
-            self.rx_code = code
-            self.rx_code_timestamp = timestamp
-            self.rx_bitlength = int(change_count / 2)
-            self.rx_pulselength = delay
-            self.rx_proto = pnum
+            self.rx_signal = Signal(timestamp, code, delay, int(change_count / 2), protocol)
             return True
 
         return False
