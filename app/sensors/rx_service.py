94bb53087c1becc77b0e4909d6ebf8a69a1d3c91
Fabio Hellmann <fhellmann93@googlemail.com>
2018-02-18 21:52:56 +0100

    Disable possibility to search multi-threaded; Add more logging

--- app/sensors/rx_service.py
+++ app/sensors/rx_service.py
@@ -18,18 +18,23 @@ class RxService:
     def __init__(self):
         self.rx_device = Device(GPIO_PI.GPIO_27.value)
         self.result = None
+        self.searching = False
 
     def __del__(self):
         self.rx_device.cleanup()
 
     def search_verified_signals(self, time_to_search_sec):
-        _LOGGER.debug("Start search for verified signals")
+        if self.searching:
+            return
         threading.Thread(target=self._listening, args=(time_to_search_sec,)).start()
 
     def get_result(self):
         return self.result
 
     def _listening(self, time_to_search_sec):
+        _LOGGER.debug("Start search for verified signals")
+
+        self.searching = True
         self.rx_device.enable_rx()
         rf_signal_counter = {}
         rf_signal_dict = {}
@@ -44,6 +49,8 @@ class RxService:
 
                 rf_signal_dict[str(rf_signal.code)] = rf_signal
                 rf_signal_counter[str(rf_signal.code)] += 1
+                _LOGGER.debug("Found signal: " + str(rf_signal))
+                _LOGGER.debug("Signal Counter: " + str(rf_signal_counter))
 
                 # Signal found -> filter only the signals we would like to see
                 if rf_signal_counter[str(rf_signal.code)] >= 3:
@@ -53,4 +60,6 @@ class RxService:
             time.sleep(0.01)
 
         self.rx_device.disable_rx()
+        self.searching = False
+
         _LOGGER.debug("Finish search for verified signals")
