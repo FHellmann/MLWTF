1bae51cbdac7e9bddb0832e907cffa5d3399ed85
Fabio Hellmann <fhellmann93@googlemail.com>
2018-02-18 21:07:13 +0100

    Code Refactoring

--- app/actuators/__init__.py
+++ app/actuators/__init__.py
@@ -3,8 +3,23 @@
     Author: Fabio Hellmann <info@fabio-hellmann.de>
 """
 
-from flask import Blueprint
+from flask import Blueprint, request, abort
+from .tx_service import TxService
 
 actuators = Blueprint('actuators', __name__)
 
-from . import api
+tx_service = TxService()
+
+
+@actuators.route('/', methods=['GET'])
+def index():
+    pass
+
+
+@actuators.route('/tx/send', methods=['POST'])
+def tx_send():
+    if not request.json:
+        abort(400)
+    # TODO Convert request.json into rf_rpi.Signal
+    # tx_service.send(signal)
+
--- app/sensors/__init__.py
+++ app/sensors/__init__.py
@@ -3,8 +3,26 @@
     Author: Fabio Hellmann <info@fabio-hellmann.de>
 """
 
-from flask import Blueprint
+from flask import Blueprint, request, abort, jsonify
+import json
+from .rx_service import RxService
 
 sensors = Blueprint('sensors', __name__)
 
-from . import api
+rx_service = RxService()
+
+
+@sensors.route('/', methods=['GET'])
+def index():
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+@sensors.route('/rx/search', methods=['PUT'])
+def rx_find():
+    rx_service.search_verified_signals(30)
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+@sensors.route('/rx/found_devices', methods=['GET'])
+def get_rx_found_devices():
+    return jsonify(rx_service.get_result())
