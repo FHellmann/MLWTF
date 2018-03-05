768691e5b04669ee0abdd6b3a727830dd9c97ba7
Fabio Hellmann <fhellmann93@googlemail.com>
2018-03-06 00:31:10 +0100

    Fix url start with leading slash

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__, static_url_path='app', static_folder='web/static', template_folder='web/templates')
+    app = Flask(__name__, static_url_path='/app', static_folder='web/static', template_folder='web/templates')
 
     from .api import register_blueprints as rest_api
     rest_api(app)
