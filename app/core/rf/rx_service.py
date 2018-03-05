cac8a057298c8397c4e916e94cab1dfe8dec2c3b
Fabio Hellmann <fhellmann93@googlemail.com>
2018-03-06 00:08:28 +0100

    Go back to continues rf signal tracing

--- app/core/rf/rx_service.py
+++ app/core/rf/rx_service.py
@@ -6,9 +6,9 @@
 import logging
 import threading
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from . import SignalList
+from . import SignalCollection
 from .rf_rpi import Device
 from ..gpio import RaspberryPi3 as GPIO_PI
 
@@ -18,50 +18,38 @@ _LOGGER = logging.getLogger(__name__)
 class RxService:
     def __init__(self):
         self.rx_device = Device(GPIO_PI.GPIO_27.value)
-        self.result = None
+        self.signal_list = []
         self.searching = False
+        threading.Thread(target=self._listening).start()
 
     def __del__(self):
         self.rx_device.cleanup()
 
-    def search_verified_signals(self, time_to_search_sec):
-        if self.searching:
-            return False
-        threading.Thread(target=self._listening, args=(time_to_search_sec,)).start()
-        return True
+    def get_results(self, since):
+        result = []
 
-    def get_result(self):
-        return self.result
+        for signal in self.signal_list:
+            if signal.time >= since:
+                result.append(signal)
 
-    def _listening(self, time_to_search_sec):
-        _LOGGER.debug("Start search for verified signals")
+        return SignalCollection(result)
 
-        self.searching = True
+    def _listening(self):
+        _LOGGER.debug("Listening for radio frequency signals")
+
         self.rx_device.enable_rx()
-        rf_signal_codes = {}
-        self.result = []
-        start_time = datetime.now()
 
         timestamp = None
-        while (datetime.now() - start_time).seconds < time_to_search_sec:
+        while True:
             rf_signal = self.rx_device.rx_signal
             if not (rf_signal is None) and rf_signal.time != timestamp:
                 timestamp = rf_signal.time
 
-                if rf_signal.code not in rf_signal_codes:
-                    rf_signal_codes[rf_signal.code] = 1
-                rf_signal_codes[rf_signal.code] += 1
+                self.signal_list.append(rf_signal)
 
-                # Signal found -> filter only the signals we would like to see
-                if rf_signal_codes[rf_signal.code] == 3:
-                    _LOGGER.debug("Found verified signal: " + str(rf_signal))
-                    self.result.append(rf_signal)
+                # To prevent a memory leak -> remove all cached entries older then 15 minutes
+                cache_time_limit = int(round(time.time() - 15 * 60))
+                while self.signal_list[0].time < cache_time_limit:
+                    self.signal_list.pop(0)
 
             time.sleep(0.01)
-
-        self.rx_device.disable_rx()
-        self.searching = False
-
-        _LOGGER.debug("Finish search for verified signals (Found=" + str(len(self.result)) + ")")
-
-        self.result = SignalList(self.result)
