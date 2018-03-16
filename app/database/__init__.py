eef31682c070315596ee02e16ac046dda9ced4ca
Fabio Hellmann <fhellmann93@googlemail.com>
2018-03-16 01:22:49 +0100

    Update database to become thread-safe

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -12,49 +12,76 @@ from datetime import datetime
 
 from .models import Event, EventType, DataSourceType
 from .converter import converter
+from app.core.threading_utils import RWLock
 
 _LOGGER = logging.getLogger(__name__)
 
 
 class Database(object):
-    def __init__(self):
-        # Default: use in-memory database
-        self.db = TinyDB(storage=MemoryStorage)
-        self.table_events = None
-        self.table_users = None
-        self.table_devices = None
-        self.setup(None)
+    class __Database(object):
+        def __init__(self):
+            # Default: use in-memory database
+            self.db = TinyDB(storage=MemoryStorage)
+            self.table_events = None
+            self.table_users = None
+            self.table_devices = None
+            self.lock = RWLock()
 
-    def __del__(self):
-        self.db.close()
+        def __del__(self):
+            self.db.close()
+
+    instance = None
+
+    def __init__(self):
+        if not self.instance:
+            self.instance = Database.__Database()
+            self.setup(None)
 
     def setup(self, path):
         if not (path is None):
-            self.db = TinyDB(path)
+            self.instance.db = TinyDB(path)
 
-        self.table_events = self.db.table('events')
-        self.table_users = self.db.table('users')
-        self.table_devices = self.db.table('devices')
+        self.instance.table_events = self.instance.db.table('events')
+        self.instance.table_users = self.instance.db.table('users')
+        self.instance.table_devices = self.instance.db.table('devices')
 
     def add_event(self, data, event_type: EventType, data_source_type: DataSourceType):
-        event = Event(event_type=event_type, data_source_type=data_source_type, data=data)
-        unstructure = converter.unstructure(event)
-        self.table_events.insert(unstructure)
-        return event
+        with self.instance.lock.writer():
+            event = Event(event_type=event_type, data_source_type=data_source_type, data=data)
+            unstructure = converter.unstructure(event)
+            self.instance.table_events.insert(unstructure)
+            return event
 
     def get_event(self, event_id: int):
-        event = self.table_events.get(doc_id=event_id)
-        return event
+        with self.instance.lock.reader():
+            event = self.instance.table_events.get(doc_id=event_id)
+            return event
 
     def get_events_by(self, event_type: EventType, data_source_type: DataSourceType,
                       since: datetime = datetime.utcnow()):
-        search = self.table_events.search(
-            (Query().event_type == event_type.value) &
-            (Query().data_source_type == data_source_type.value) &
-            (Query().timestamp >= since.timestamp())
-        )
-        structure = converter.structure(search, List[Event])
-        return structure
+        with self.instance.lock.reader():
+            search = self.instance.table_events.search(
+                (Query().event_type == event_type.value) &
+                (Query().data_source_type == data_source_type.value) &
+                (Query().timestamp >= since.timestamp())
+            )
+            structure = converter.structure(search, List[Event])
+            return structure
+
+    def get_last_event(self, event_type: EventType, data_source_type: DataSourceType):
+        with self.instance.lock.reader():
+            event_count = len(self.instance.table_events)
+
+            global last_event
+            while True:
+                last_event_raw = self.instance.table_events.get(doc_id=event_count)
+                last_event = converter.structure(last_event_raw, Event)
+                event_count -= 1
+                if (last_event.event_type == event_type and last_event.data_source_type == data_source_type) or \
+                    event_count < 1:
+                    break
+
+            return last_event
 
 
 db = Database()
