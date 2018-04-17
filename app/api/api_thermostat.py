bd2da7b9f9f54475e0abc65bfe6b2945da3b21c2
Fabio Hellmann <fhellmann93@googlemail.com>
2018-04-17 10:02:39 +0200

    Add thermostat scan to rest api

--- app/api/api_thermostat.py
+++ app/api/api_thermostat.py
@@ -6,7 +6,7 @@
 from flask_restplus import Namespace, Resource, reqparse
 from marshmallow import Schema, fields as ma_fields, post_load
 
-from app.core.bluetooth import bt_controller, BLEDevice
+from app.core.thermostat import thermostat_controller, Thermostat, ThermostatManufacturer
 
 ns_ts = Namespace('thermostat', description='The thermostat interface')
 
@@ -14,18 +14,28 @@ get_parser = reqparse.RequestParser()
 get_parser.add_argument('since', type=int)
 
 
-class BTDeviceSchema(Schema):
-    mac = ma_fields.String()
+class ThermostatManufacturerSchema(Schema):
+    name = ma_fields.String()
 
     @post_load
-    def create_bt_device(self, data):
-        return BLEDevice(**data)
+    def create_protocol(self, data):
+        return ThermostatManufacturer(**data)
+
+class ThermostatSchema(Schema):
+    addr = ma_fields.String()
+    name = ma_fields.String()
+    issr = ma_fields.String()
+    manufacturer = ma_fields.Nested(ThermostatManufacturerSchema())
+
+    @post_load
+    def create(self, data):
+        return Thermostat(**data)
 
 
 @ns_ts.route('/all')
-class BluetoothDeviceCollectionResource(Resource):
+class ThermostatCollectionResource(Resource):
 
     def get(self):
-        schema = BTDeviceSchema(many=True)
-        result = bt_controller.scan()
+        schema = ThermostatSchema(many=True)
+        result = thermostat_controller.scan()
         return schema.dump(result)
